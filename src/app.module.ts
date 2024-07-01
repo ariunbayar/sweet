f69d76f0ebf80be96ad8d48e8dbebc36317962bc
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2024-07-01 15:09:53 -0300

    #8 Database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: false,  // Using migrations instead
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
